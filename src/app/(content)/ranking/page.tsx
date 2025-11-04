90d9d88e6834420d8245cecfde251e71369eb4fd
KrzyMru <krzymru20@gmail.com>
2025-11-04 15:05:13 +0100

    Ranking page styling

--- src/app/(content)/ranking/page.tsx
+++ src/app/(content)/ranking/page.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import { CountryDataRanking } from "./types";
+import RankingTable from "./components/ranking-table/ranking-table";
 
 const Page = async () => {
   const data = await fetch("https://restcountries.com/v3.1/all?fields=cca3,name,region,subregion,population,area,flags,independent,unMember", {
@@ -12,41 +12,19 @@ const Page = async () => {
   const countries: CountryDataRanking[] = await data.json();
 
   return (
-    <div>
-      <div>
-        <span>Found 234 countries</span>
+    <div className={styles.page}>
+      <header className={styles.header}>
+        <span className="text__lg--semibold">Found {countries.length} countries</span>
         <div>Search bar</div>
+      </header>
+      <div className={styles.content}>
+        <div className={styles.filters}>
+          <div>Sorting</div>
+          <div>Region filters</div>
+          <div>Status filters</div>
+        </div>
+        <RankingTable countries={countries} />
       </div>
-      <div>
-        <div>Sorting</div>
-        <div>Region filters</div>
-        <div>Status filters</div>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th scope="col">Flag</th>
-            <th scope="col">Name</th>
-            <th scope="col">Population</th>
-            <th scope="col">Area(km<sup>2</sup>)</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td scope="row">
-              <Image
-                src='https://flagcdn.com/w320/de.png'
-                alt=''
-                width={56}
-                height={28}
-              />
-            </td>
-            <td scope="row">Germany</td>
-            <td scope="row">83491249</td>
-            <td scope="row">357114</td>
-          </tr>
-        </tbody>
-      </table>
     </div>
   );
 }
