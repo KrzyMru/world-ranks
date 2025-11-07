d8f63cb520b4e491009c32b01ee6d2e17863ee71
KrzyMru <krzymru20@gmail.com>
2025-11-07 14:03:55 +0100

    Loading page for the country ranking

---
+++ src/app/(content)/ranking/loading.tsx
@@ -0,0 +1,57 @@
+import styles from "./loading.module.css";
+import { regions, statuses } from "./types";
+
+const Loading = () => {
+    return (
+        <div className={styles.page}>
+            <div className={styles.header}>
+                <div className={styles.header__text} />
+                <div className={styles.header__search} />
+            </div>
+            <div className={styles.content}>
+                <div className={styles.filters}>
+                    <div className={styles.filters__content}>
+                        <div className={styles.filters__text} />
+                        <div className={styles.filters__sort} />
+                    </div>
+                    <div className={styles.filters__content}>
+                        <div className={styles.filters__text} />
+                        <div className={styles.filters__regions}>
+                            {
+                                regions.map(region => 
+                                    <div 
+                                        key={region}
+                                        className={`${styles.filters__region} text__sm--bold noselect`}
+                                    >
+                                        {region}
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </div>
+                    <div className={styles.filters__content}>
+                        <div className={styles.filters__text} />
+                        <div className={styles.filters__statuses}>
+                            {
+                                statuses.map(status => 
+                                    <div 
+                                        key={status.property}
+                                        className={`${styles.filters__status} text__sm--bold noselect`}
+                                    >
+                                        {status.title}
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.ranking}>
+                    <div className={styles.ranking__header} />
+                    <div className={styles.ranking__content} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Loading;
